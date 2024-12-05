d31fd41f84cc84739f1f72a85f9845b800464995
Monthol Changsathien <monthol@macbookpro.home>
2024-12-04 20:08:43 -0500

    Add images

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -81,13 +81,13 @@ const Sidebar = () => {
           isSidebarCollapsed ? "px-5" : "px-8"
         }`}
       >
-        <Image
-          src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/logo.png"
-          alt="edstock-logo"
-          width={27}
-          height={27}
-          className="rounded w-8"
-        />
+            <Image
+              src="https://s3-inv-mng.s3.us-east-1.amazonaws.com/logo.jpg"
+              alt="Logo"
+              width={27}
+              height={27}
+              className="rounded w-8"
+            />
         <h1
           className={`${
             isSidebarCollapsed ? "hidden" : "block"
