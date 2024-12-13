5719a1f15fb4de82e36a45156a2208d618b3b8cb
Monthol Changsathien <monthol@macbookpro.home>
2024-12-12 19:08:26 -0500

    Change

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3-inv-mng.s3.us-east-1.amazonaws.com",
+        hostname: "s3-ims-capstone.s3.us-east-1.amazonaws.com",
         port: "",
         pathname: "/**",
       },
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -81,7 +81,7 @@ const Products = () => {
             >
               <div className="flex flex-col items-center">
                 <Image
-                  src={`https://s3-inv-mng.s3.us-east-1.amazonaws.com/product${
+                  src={`https://s3-ims-capstone.s3.us-east-1.amazonaws.com/product${
                     Math.floor(Math.random() * 3) + 1
                   }.png`}
                   alt={product.name}
